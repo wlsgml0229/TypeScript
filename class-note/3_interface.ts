56fdea3bd774c9575f9fc52cc53cd924b7242f3b
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2022-08-17 23:21:19 +0900

    인터페이스 추가 정리 - 함수 구조, 인덱싱, 딕셔너리, 상속

--- class-note/3_interface.ts
+++ class-note/3_interface.ts
@@ -26,3 +26,60 @@ const capt = {
 //any 일때는 어떤 데이터를 넣어도 무방
 //인자가 파라미터에 정의한 인터페이스를 잘 따르고 있는 지 확인!
 getUser(capt);
+
+// 함수의 스펙(구조)에 인터페이스를 활용
+// -> 함수의 전체적인 모습을 정의 할 수 있다.
+interface SumFunction {
+    (a: number, b: number): number;
+}
+
+
+let sum: SumFunction;
+sum = function(a:number, b:number) {
+    return a + b;
+}
+
+//인덱싱
+interface StringArray {
+    [index: number]: string;
+}
+
+let arr = ['a','b','c'];
+// 1. 인덱스로 접근
+//2. 배열의 방식을 인터페이스로 정할수 있다.
+// arr[0] = 10; // 'a'
+
+//딕셔너리 패턴 
+interface StringRegexDictionary {
+    //정규표현식 생성자 예약어 제공 (타입제공)
+    [key: string]: RegExp
+}
+
+let obj: StringRegexDictionary = {
+    // sth: /abc/,
+    //오브젝트에 인터페이스를 정의한 순간 형식에 안맞는것들은 모두 에러 발생
+    cssFile: /\.css$/,
+    jsFile: /\.js$/,
+}
+//에러발생
+// obj['cssFile'] = 'a' 
+
+// 객체의 키를 접근할때 값에대한 타입추론이 가능함
+Object.keys(obj).forEach((value) => {})
+
+// 인터페이스의 확장
+interface Person {
+    name: string;
+    age: number;
+}
+
+//상속을 받아 확장이 가능
+interface Developer extends Person {
+    langauage: string;
+}
+
+let jini: Developer = {
+    name: 'jin',
+    age: 27,
+    langauage: 'ts'
+}
