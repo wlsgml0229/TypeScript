87ad360bf9adfb7b8b6212df68899747aba6cb6c
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2022-08-21 16:10:18 +0900

    union 으로 사용했을때의 문제점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -18,21 +18,36 @@
 // !!!기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복선언의 단점
 
 // 타입을 다르게 받기 위해 새로운 함수를 만들어내야함. ==> 유지보수 관점에서 좋지않음 코드가 방대해짐
-function logText(text: string) {
-    console.log(text);
-//    text.split('').reverse().join('');
-    return text;
-}
+// function logText(text: string) {
+//     console.log(text);
+// //    text.split('').reverse().join('');
+//     return text;
+// }
 
-function logNumber(num: number) {
-    console.log(num);
-    return num;
-}
+// function logNumber(num: number) {
+//     console.log(num);
+//     return num;
+// }
 
-logText(10); // 숫자 10
-const num = logNumber(10);
-logText('하이'); // 문자열 하이
-logText(true); // 진위값 true 
+// logText(10); // 숫자 10
+// const num = logNumber(10);
+// logText('하이'); // 문자열 하이
+// logText(true); // 진위값 true 
+
+
+// !! union으로 사용했을때 문제점
+// function logText(text: string | number) {
+//     console.log(text);
+//     text // string, number의 교집합으로 두 타입이 모두 접근가능한 속성 또는 api에 관해서만 프리뷰를 제공 하게되는 문제점
+//     return text;
+// }
+
+// const a = logText('a');
+// //리턴값을 받을때 a에 관한 값도 스트링과 넘버타입을 받게됨 - split 사용 불가
+// a.split()
+// logText(10);
+
+
 
 
 
