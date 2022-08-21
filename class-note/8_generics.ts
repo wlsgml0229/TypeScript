8b447a779ee45c6413903101ac60e9c68daa91ec
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2022-08-21 15:59:46 +0900

    기존 타입 정의 방식으로 사용시 함수 중복선언에 대한 문제점

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -8,10 +8,31 @@
 // logText('하이'); // 문자열 하이
 // logText(true); // 진위값 true 
 
-function logText<T>(text: T):T {
+// function logText<T>(text: T):T {
+//     console.log(text);
+//     return text;
+// }
+
+// logText<string>('하이'); //호출하는 시점에 타입을 넘겨줄 수 있는것이 제네릭의 특징
+
+// !!!기존 타입 정의 방식과 제네릭의 차이점 - 함수 중복선언의 단점
+
+// 타입을 다르게 받기 위해 새로운 함수를 만들어내야함. ==> 유지보수 관점에서 좋지않음 코드가 방대해짐
+function logText(text: string) {
     console.log(text);
+//    text.split('').reverse().join('');
     return text;
 }
 
-logText<string>('하이'); //호출하는 시점에 타입을 넘겨줄 수 있는것이 제네릭의 특징
+function logNumber(num: number) {
+    console.log(num);
+    return num;
+}
+
+logText(10); // 숫자 10
+const num = logNumber(10);
+logText('하이'); // 문자열 하이
+logText(true); // 진위값 true 
+
+
 
