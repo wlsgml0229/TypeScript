8bff14bf51901fb57967fb72e9354bf339fe34e4
wlsgml961010@gmail.com <wlsgml0229@naver.com>
2022-08-21 15:32:47 +0900

    인터페이스 활용한 연산자

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,3 +1,5 @@
+
+// any를 쓰면 타입스크립트의 장점이 사라짐 - 타입스크립트가 추론하는 시점에서 any로 인식
 // function logMessage(value: any) {
 //     console.log(value);
 //     value
@@ -7,7 +9,6 @@
 
 
 // 유니온 타입 - 한가지타입 이상을 쓸수 있게 만든다.
-// any를 쓰면 타입스크립트의 장점이 사라짐 - 타입스크립트가 추론하는 시점에서 any로 인식
 // string 과 number 둘다 허용
 var seho: string | number | boolean;
 
@@ -22,3 +23,42 @@ function logMessage(value: string | number) {
 }
 logMessage('hello');
 logMessage(100);
+
+interface Developer {
+    name: string;
+    skill: string;
+}
+
+interface Person {
+    name: string;
+    age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+    // someone 은 함수내부에서 적용할 수 있는 속성은 name뿐임
+    // 유니온속성을 적용하게 되면 Developer 또는 Person에 모두 적용할 수 있어야하기 떄문에 공통적 속성인 name 민 가능
+    someone.name
+
+    //접근 x
+    //skill 과 age에 접근하고 싶다면 타입가드를 이용하여 추가 접근 가능하게 됨
+    // someone.skill
+    // someone.age
+}
+//각각의 Developer, Person에만 맞는 객체를 넘기면 됨
+askSomeone({name: '디벨로퍼', skill: '웹 개발'});
+askSomeone({name: '캡틴', age: 100})
+
+// && 인터섹션타입
+let jin: string | number | boolean;
+// never
+let cap: string & number & boolean;
+// function askSomeone(someone: Developer & Person) {
+//     //타입가드가 필요없이 Developer와 Person 의 속성 접근이 가능하게됨
+//     someone.name
+//     someone.skill
+//     someone.age
+// }
+// //developer 와 person 의 타입까지 다 합한 (합집합) 객체를 넘겨야됨 
+// askSomeone({name: '캡틴', 'age': 100})
+
+
